34f1dcdcf5154e6dc02ead369af35b3a5da3387b
Rosita04 <rocioramirez@knights.ucf.edu>
2017-04-10 16:28:44 -0400

    I added pics

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+//'use strict';
+
+// initialize fullPage
+
+$(document).ready(function () {
+  $('#fullpage').fullpage({
+    verticalCentered: false,
+
+    //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
+    css3: false
+
+  });
+});
+//# sourceMappingURL=main.js.map
+
+//TRYING TO APPLY THREE.JS LIBRARY
+
